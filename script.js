69837be99112f984a5cdfd23dbe0fa2972c92024
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-06-16 16:56:47 -0300

    alterado css

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const email = document.querySelector('.login');
 const password = document.querySelector('.senha');
 const bntLogin = document.querySelector('#buttonLogin');
 
-bntLogin.addEventListener('click', function check() {
-  if(email.value == 'tryber@teste.com' && password.value == '123456') {
+bntLogin.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
-  } else { 
+  } else {
     alert('Login ou senha inválidos.');
-  };
+  }
 });
