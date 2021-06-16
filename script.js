a49e4fb847c9e6b5f6913e525ee5a2a2bd7defd4
thomasravache <thomasravache31@gmail.com>
2021-06-16 18:22:57 -0300

    Requisito 18 --> OK

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const btnLogar = document.querySelector('#btn-logar');
 const txtLogin = document.querySelector('#login');
 const txtSenha = document.querySelector('#senha');
+const btnEnviar = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
+
+btnEnviar.disabled = true;
 
 btnLogar.addEventListener('click', function validarLogin() {
   if (txtLogin.value === 'tryber@teste.com' && txtSenha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+
+  return this;
+});
+
+agreement.addEventListener('click', function validateEnviar(event) {
+  if (event.target.checked) {
+    btnEnviar.disabled = false;
+  } else {
+    btnEnviar.disabled = true;
   }
 
   return this;
