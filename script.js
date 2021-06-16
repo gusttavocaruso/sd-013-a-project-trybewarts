58846f2cff81a92c09f238408d8b3026ce0f15b1
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-06-16 20:12:24 -0300

    alinhando os requisitos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+/* Variaveis */
 let btnLogin = document.getElementById("btnlogin");
 let inputLogin = document.getElementById("login")
 let inputSenha = document.getElementById("senha")
 
-
+/* função validar login */
 btnLogin.addEventListener("click", () => {
   if(inputLogin.value === 'tryber@teste.com' || inputSenha.value === '123456'){
     alert("Olá, Tryber!");
@@ -11,12 +11,3 @@ btnLogin.addEventListener("click", () => {
       alert("Login ou senha inválidos.");
     }
 });
-
-
-// buttonEntrar.addEventListener('click', () => {
-//   if (login.value === 'tryber@teste.com' && password.value === '123456') {
-//     alert('Olá, Tryber!');
-//   } else {
-//     alert('Login ou senha inválidos.');
-//   }
-// });
