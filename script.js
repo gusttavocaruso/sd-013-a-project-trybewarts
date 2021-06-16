6ed41554dca9297d6c37e21ce342cee9a1dc2d0d
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-06-16 20:04:20 -0300

    Corrige lint

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ buttonL.addEventListener('click', (e) => {
   const login = document.getElementById('login');
   const senha = document.getElementById('senha');
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
