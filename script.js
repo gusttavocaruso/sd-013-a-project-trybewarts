a23252ca7c1b2225cab44a8e660ce6bb6030cfa3
Gabriel Lenz <lenzordmusic@gmail.com>
2021-06-16 15:47:02 -0400

    Requisito 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonForm = document.getElementById('login-button')
+buttonForm.addEventListener('click', () => {
+  const loginValue = document.getElementById('form-login').value;
+  const passwordValue = document.getElementById('form-password').value;
+  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
+    alert('Olá, Tryber!')
+  } else {
+    alert('Login ou senha inválidos.')
+  }
+})
+
