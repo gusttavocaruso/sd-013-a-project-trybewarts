f635d80bcfaf3cf4d04ece09ca6d9a4052fe67ba
Micael Santos <micaelmaicon@hotmail.com>
2021-06-16 19:23:19 -0300

    Requirement 4

---
+++ script.js
@@ -0,0 +1,21 @@
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+const btnLogin = document.getElementById('btn-logar');
+const loginResister = 'tryber@teste.com';
+const passwordResister = '123456';
+const msgValidate = 'Olá, Tryber!';
+const msgNoValidate = 'Login ou senha inválidos!';
+
+function showAlert(msg) {
+  alert(msg);
+}
+
+function showLogin() {
+  if (login.value === loginResister && password.value === passwordResister) {
+    showAlert(msgValidate);
+  } else {
+    showAlert(msgNoValidate);
+  }
+}
+
+btnLogin.addEventListener('click', showLogin);
