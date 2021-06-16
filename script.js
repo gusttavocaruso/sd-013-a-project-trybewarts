d0fb3c25c7e9ff7b0b772d431e4c07f7c6ecacca
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-16 15:55:33 -0300

    Adiciona requisitos 1 ao 4

---
+++ script.js
@@ -0,0 +1,12 @@
+const inpuLogin = document.querySelector('#input-login');
+const inpuSenha = document.querySelector('#input-senha');
+const buttonEnviar = document.querySelector('#button-enviar');
+
+function VerificaLogin() {
+  const login = inpuLogin.value;
+  const senha = inpuSenha.value;
+  if (login === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {alert('Login ou senha inválidos.')}
+}
+buttonEnviar.addEventListener('click',VerificaLogin)
