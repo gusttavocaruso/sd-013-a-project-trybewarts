292f9e86340c027d18ea0ecbf470d782a3b0dc6e
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-16 20:52:04 -0300

    Req 14

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttonForm = document.getElementById('login-button');
+const rateField = document.querySelector('.rate-field');
 buttonForm.addEventListener('click', () => {
   const loginValue = document.getElementById('form-login').value;
   const passwordValue = document.getElementById('form-password').value;
@@ -8,3 +9,17 @@ buttonForm.addEventListener('click', () => {
     alert('Login ou senha inválidos.');
   }
 });
+
+function setRateField() {
+  for (let i = 0; i < 10; i += 1) {
+    const newRate = document.createElement('input');
+    const spanValue = document.createElement('span');
+    newRate.type = 'radio';
+    newRate.name = 'rate';
+    newRate.value = i + 1;
+    spanValue.innerHTML = i + 1;
+    rateField.appendChild(newRate);
+    rateField.appendChild(spanValue);
+  }
+}
+setRateField();
