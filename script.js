8d6988af50ec6e83072d1902cf7299ab8e53dc9a
Tatiane Chiarello <tatichiarello@gmail.com>
2021-06-16 21:17:38 +0200

    requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputLogin = document.querySelector("#input-login");
+const inputSenha = document.querySelector("#input-senha");
+const inputButton = document.querySelector("#login-button");
+
+inputButton.addEventListener("click", () => {
+  const login = "tryber@teste.com";
+  const senha = "123456";
+
+  if (inputLogin.value !== login || inputSenha.value !== senha) {
+    alert("Login ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+});
