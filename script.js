5d3a70c82169f58f4c18c5f080d801b119ced9c4
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-16 19:02:30 -0300

    add ESlint updates

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function funcaoLogin() {
   const login = document.getElementById('login').value;
   const senha = document.getElementById('senha').value;
   if (login === 'tryber@teste.com' && senha === '123456') {
-    alert('Olá, Tryber!')
+    alert('Olá, Tryber!');
   } else {
-    alert('Login ou senha inválidos.')
+    alert('Login ou senha inválidos.');
   }
 }
