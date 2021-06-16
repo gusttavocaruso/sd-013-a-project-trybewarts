d1a3320ddb32c488a329c4e24be9ad8e7684cc91
Wesley Maia <maia.weol@gmail.com>
2021-06-16 20:00:21 -0300

    Refatorando o arquivo JS

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const getButton = document.getElementById('btn-enter');
-const getLogin =  document.getElementById('input-login');
-const getSenha =  document.getElementById('input-password');
+const getLogin = document.getElementById('input-login');
+const getSenha = document.getElementById('input-password');
 const login = 'tryber@teste.com';
 const senha = '123456';
 
 getButton.addEventListener('click', () => {
-  if ( (getLogin.value !== login) && (getSenha.value !== senha)) {
-    alert ('Login ou senha inválidos.');
+  if ((getLogin.value !== login) && (getSenha.value !== senha)) {
+    alert('Login ou senha inválidos.');
   } else {
-    alert ("Olá, Tryber!");
+    alert('Olá, Tryber!');
   }
-})
+});
