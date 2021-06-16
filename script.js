1073c152b53b4127cda800cc888093b943f95aa9
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-06-16 19:14:55 -0300

    Segunda correçao Requisito 3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const buttonL = document.getElementById('login-button');
+const buttonL = document.getElementById('entrar-button');
 
 buttonL.addEventListener('click', (e) => {
   e.preventDefault();
   const login = document.getElementById('login');
   const senha = document.getElementById('senha');
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber!');
+    alert("Olá, Tryber!");
   } else {
     alert('Login ou senha inválidos.');
   }
