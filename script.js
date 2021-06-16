35fd600f3fecec8772faf6c43087a4994c7e656d
Micael Santos <micaelmaicon@hotmail.com>
2021-06-16 20:14:21 -0300

    Requirement 5

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const btnLogin = document.getElementById('btn-logar');
 const loginResister = 'tryber@teste.com';
 const passwordResister = '123456';
 const msgValidate = 'Olá, Tryber!';
-const msgNoValidate = 'Login ou senha inválidos!';
+const msgNoValidate = 'Login ou senha inválidos';
 
 function showAlert(msg) {
   alert(msg);
 }
 
 function showLogin() {
-  if (login.value === loginResister && password.value === passwordResister) {
+  if ((login.value === loginResister) && (password.value === passwordResister)) {
     showAlert(msgValidate);
   } else {
     showAlert(msgNoValidate);
