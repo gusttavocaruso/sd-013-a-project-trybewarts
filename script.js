b59ab7ac9da1c498bccc3a88e5e0eb4de25d3ae7
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-06-16 16:24:15 -0300

    Adiciona requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,23 @@
+const loginInput = document.getElementById('login');
+const passInput = document.getElementById('pass');
+const loginButton = document.getElementById('login-bt');
+
+const dataLogin = {
+  login: 'tryber@teste.com',
+  pass: '123456'
+}
+
+function loginAuthentication() {
+
+  const loginText = loginInput.value;
+  const passText = passInput.value;
+
+  if (dataLogin.login === loginText && dataLogin.pass === passText) {
+    alert('Olá, Tryber!')
+  } else {
+    alert('Login ou senha inválidos.')
+  }
+}
+
+loginButton.onclick = loginAuthentication;
+
