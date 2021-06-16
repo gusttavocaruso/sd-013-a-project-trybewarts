cf3f7cda3c88f878e1f18243e093824e94413320
Murilo Rainho <murilorainho01@gmail.com>
2021-06-16 17:48:18 -0300

    Até o 13 okay

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const login = document.querySelector('#email');
 const password = document.querySelector('#password');
 const buttonEntrar = document.querySelector('#entrar');
 // const formEvaluation = document.querySelector('#evaluation-form');
+// const buttonAgreement = document.querySelector('#agreement');
+// const enviarFormEvaluation = document.querySelector('#submit-btn');
+// const counterTextArea = document.querySelector('#counter');
+// let count = 500;
+// let lenghtDoText;
 
 // Verifica o email e a senha
 buttonEntrar.addEventListener('click', () => {
@@ -14,3 +19,22 @@ buttonEntrar.addEventListener('click', () => {
     alert('Login ou senha inválidos.');
   }
 });
+
+// Verifica se o checkbox 16 foi selecionado para, só então, o botão enviar funcionar
+// enviarFormEvaluation.addEventListener('click', (event) => {
+//   if (buttonAgreement.value !== 'on') {
+//     event.preventDefault();
+//   }
+// });
+
+// Cria o contador de caracteres da textArea
+// addEventListener('input', () => {
+//   count -= 1;
+// });
+
+// addEventListener('keypress', (event) => {
+//   if (event.keyCode === 8 || event.keyCode === 46) {
+//     if (event.lenght )
+//     count += 1;
+//   }
+// });
