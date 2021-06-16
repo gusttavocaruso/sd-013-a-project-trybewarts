47cc463f43ebb84b6c978926d56ae0dc73f0ac0d
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-06-16 19:36:09 -0300

    Requisitos até o 3

---
+++ script.js
@@ -0,0 +1,15 @@
+function check() {
+  const login = document.getElementById('login').value;
+  const senha = document.getElementById('senha').value;
+  const emailDefault = 'tryber@teste.com';
+  const senhaDefault = '123456';
+
+  if ( login === emailDefault && senha === senhaDefault) {
+      alert('Olá, Tryber!');
+  } else {
+      alert('Login ou senha inválidos.');
+  }
+}
+
+const botao = document.getElementById('botao');
+botao.addEventListener('click', check);
