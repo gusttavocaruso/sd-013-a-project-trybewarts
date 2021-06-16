74e6dae14a9bdd7e34f223948bee77e3bf222193
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-16 19:49:11 -0300

    ESlint updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // função responsavel pelo requisito numero 3
-let logBtn = document.getElementById('log-btn');
+const logBtn = document.getElementById('log-btn');
 
-logBtn.addEventListener('click', function funcaoLogin() {
+logBtn.addEventListener('click', () => {
   const login = document.getElementById('login').value;
   const senha = document.getElementById('senha').value;
   if (login === 'tryber@teste.com' && senha === '123456') {
