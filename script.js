f131d183cd9d4a4bc6bc406227180d0e98768e01
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-16 17:21:03 -0300

    tentando dnv

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bntLogin = document.querySelector('#buttonLogin');
 bntLogin.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
-  } else { 
+  } else {
     alert('Login ou senha inválidos.');
   }
 });
