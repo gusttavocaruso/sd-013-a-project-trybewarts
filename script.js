df47c9123bca820e3e3aecfce98c0c84a0748d5f
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-16 18:16:28 -0300

    Ajeitando o lint

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Puxa o botão de aceitar os termos
-let agreement = document.getElementById('agreement');
+const agreement = document.getElementById('agreement');
 // Puxa o botão de enviar
-let submit = document.getElementById('submit-btn');
+const submit = document.getElementById('submit-btn');
 // Habilita o botão de enviar ao aceitar os termos
-agreement.onchange = function () {
-  if (submit.disabled == true){
-  submit.disabled = false;
+agreement.onchange = () => {
+  if (submit.disabled === true) {
+    submit.disabled = false;
   } else {
     submit.disabled = true;
   }
-}
+};
