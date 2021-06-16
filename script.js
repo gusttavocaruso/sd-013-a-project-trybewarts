a363b8a76dccb787604a7ef0e217712a1a77fa5a
Gessé Carlos <gesse.dev@outlook.com>
2021-06-16 18:53:14 -0300

    feito ate o requisito 15

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getEmailInput = document.querySelector('#email');
 const getPasswordInput = document.querySelector('#password');
-const getLoginButton = document.querySelector('input-login-button');
+const getLoginButton = document.querySelector('#input-login-button');
+
 const loginValidator = () => {
   if (getEmailInput.value === 'tryber@teste.com' && getPasswordInput.value === '123456') {
     alert('Olá, Tryber!');
@@ -9,3 +10,22 @@ const loginValidator = () => {
   }
 };
 getLoginButton.addEventListener('click', loginValidator);
+
+const trybewartsRating = () => {
+  const getRatingContainer = document.querySelector('.rating-container');
+
+  for(let i = 1; i <= 10; i += 1) {
+    const setInputNumber = document.createElement('input');
+    setInputNumber.type = 'radio';
+    setInputNumber.value = i;
+    setInputNumber.id = `rating${i}`;
+    setInputNumber.name = 'rate';
+
+    const setInputLabel = document.createElement('label');
+    setInputLabel.innerHTML = `<label for="rating${i}" id="label${i}">${i}</label>`
+
+    getRatingContainer.appendChild(setInputNumber);
+    getRatingContainer.appendChild(setInputLabel);
+  }
+}
+trybewartsRating();
