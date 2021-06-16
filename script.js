a2296cf9c021f120c0c8a76c0563661c6dd6e6e1
CarlosLima <carlos.augustolimaf@gmail.com>
2021-06-16 16:39:27 -0300

    Adiciona do requisito 1 ao 8

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttonHeader = document.getElementById('button-submit-header');
+const inputLogin = document.getElementById('login-input');
+const inputSenha = document.getElementById('senha-input');
+buttonHeader.addEventListener('click', verificarFormHeader);
+function verificarFormHeader() {
+    const login = inputLogin.value;
+    const senha = inputSenha.value;
+    if (senha === "123456" & login === "tryber@teste.com") {
+      alert("Olá, Tryber!");
+    } else {
+      alert("Login ou senha inválidos");
+    }
+}
