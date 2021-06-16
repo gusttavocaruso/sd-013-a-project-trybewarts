b5d5cedffbe11b0e9395112a87d575703a7c895b
Murilo Rainho <murilorainho01@gmail.com>
2021-06-16 20:57:20 -0300

    Testando git merge

--- script.js
+++ script.js
@@ -37,14 +37,6 @@ function inserirContador() {
 
 inserirContador();
 
-// textArea.addEventListener('keydown', (event) => {
-//   if (textArea.value !== '') {
-//     if ((event.keyCode === 8 || event.keyCode === 46)) {
-//       count += 2;
-//     }
-//   }
-// });
-
 textArea.addEventListener('input', () => {
   count = 500 - textArea.value.length;
   inserirContador();
