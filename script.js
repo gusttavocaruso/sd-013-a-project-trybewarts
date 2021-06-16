a4ec5c6233b081f68524d47526c62e935593780e
ALINE <hoshinoaline@yahoo.com.br>
2021-06-16 17:11:30 -0300

    Requisito 1 a 5

---
+++ script.js
@@ -0,0 +1,13 @@
+const login= document.getElementById('login');
+const senha= document.getElementById('senha');
+const buttonLogin=document.getElementById('buttonLogin');
+
+function validation(){
+  if ((login.value !== 'tryber@teste.com') && (senha.value !== '123456')){
+    alert('Login ou senha inválidos.');
+  }else{
+    alert('Olá, Tryber!');
+  }
+}
+
+buttonLogin.addEventListener('click', validation);
