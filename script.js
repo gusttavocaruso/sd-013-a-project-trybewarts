c5e6e6452bfdb6fa0c16c7d49c24810f95649f75
Rafael Victor <rafavictorgs@gmail.com>
2021-06-16 20:07:40 -0300

    Faz mudanças sugeridas no Codereview e corrige erros apontados pelo Linter

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const getBtnLogin = document.querySelector('#btn-login');
 getBtnLogin.addEventListener('click', () => {
   const getInputLogin = document.querySelector('#input-login');
   const getInputSenha = document.querySelector('#input-senha');
-  if (getInputLogin.value !== 'tryber@teste.com' || getInputSenha.value !== '123456') {
+  if ((getInputLogin.value) !== 'tryber@teste.com' || (getInputSenha.value !== '123456')) {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
