0990229c7223890bc6ac09b4a84ca8837ded155a
VictorDizne <victoralbertocostadiniz@gmail.com>
2021-06-16 16:11:02 -0300

    Corrige o lint do js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ loginButton.addEventListener('click', () => {
   const login = document.getElementById('login');
   const senha = document.getElementById('senha');
   const senhaCorreta = '123456';
-  const loginCorreto = 'tryber@teste.com'
+  const loginCorreto = 'tryber@teste.com';
   if (login.value !== loginCorreto && senha.value !== senhaCorreta) {
     alert('Login ou senha inválidos.');
   } else {
