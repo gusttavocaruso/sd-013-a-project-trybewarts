948a3853cd8ed67c0589d81d2a2dc077274950ae
Danilo Uehara <daniloyuno@hotmail.com>
2021-06-16 18:33:03 -0300

    arrumando erros

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-window.onload = function {
-    let login = 
-
+window.onload = function () {   
+    let login = document.querySelector("input[name=login");
+    let senha = document.querySelector("input[name=senha");
+    let pressButton = document.querySelector("button[name=button");
+    pressButton.addEventListener("click", ()=>{
+        if (login.value == "tryber@teste.com" && senha.value == "123456"){
+            alert("Olá, Tryber!");
+        }
+        else {
+            alert("Login ou senha inválidos.");
+        }
+    })    
 }
