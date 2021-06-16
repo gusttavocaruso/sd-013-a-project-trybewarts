d2a51fdc39ec38e0b56cd9c8bd1774d8e92dd527
Matheus <matheushtc25@gmail.com>
2021-06-16 20:24:06 -0300

    Finalizando os requisitos obrigatórios

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ buttonHeader.addEventListener('click', verificarFormHeader);
 function verificarFormHeader() {
     const login = inputLogin.value;
     const senha = inputSenha.value;
-    if (senha === "123456" & login === "tryber@teste.com") {
+    if ((senha === "123456") && (login === "tryber@teste.com")) {
       alert("Olá, Tryber!");
     } else {
       alert("Login ou senha inválidos.");
     }
 }
+
+document.getElementById("submit-btn").disabled = true;
+document.getElementById("agreement").addEventListener("input", function(event){
+    var conteudo = document.getElementById("agreement").value;
+    if (conteudo !== null && conteudo !== '') {
+      document.getElementById("submit-btn").disabled = false;
+    } else {
+      document.getElementById("submit-btn").disabled = true;
+    }
+});
