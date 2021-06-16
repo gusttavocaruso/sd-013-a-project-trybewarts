38d0082fc0f5d9a425a8ed30197ef353a2b2a760
Cassyel <cassyel1967@gmail.com>
2021-06-16 18:37:50 -0300

    Corrige ESlint

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /* validação de form loguin  */
-let email = document.getElementById('email-input');
-let senha = document.getElementById('senha-input');
+const email = document.getElementById('email-input');
+const senha = document.getElementById('senha-input');
 
-let botao = document.getElementById('btn');
-botao.addEventListener ('click', validationLogin);
+const botao = document.getElementById('btn');
 
-function validationLogin(){
-  if (email.value === 'tryber@teste.com' && senha.value === '123456'){ 
-    alert('Olá, Tryber!')
-  } else { 
-    alert('Login ou senha inválidos.')}
+function validationLogin() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 }
 
-
+botao.addEventListener('click', validationLogin);
