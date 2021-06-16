456e25740733e0c1564b7db60e7433b69c785342
Danilo Uehara <daniloyuno@hotmail.com>
2021-06-16 19:40:45 -0300

    add 13-14

--- script.js
+++ script.js
@@ -9,4 +9,29 @@ window.onload = function () {
       alert('Login ou senha inválidos.');
     }
   });
+  const ajuda = document.getElementsByClassName('satisfação');
+
+
+/*
+function a(){  
+  for (let aux=0; aux < 10; aux+=1){
+    let label = criar('Label');
+    ajuda.appendChield(label);
+    for (let aux2=0; aux2 < 10; aux+=1){
+      let input = criar('Input');
+      input.Classname ='rate'; 
+      rate.lastElementChild(input);     
+      ajuda.appendChild(input);
+
+    }
+  }
+}
+  function criar (nome){
+      let elemento = document.createElement(nome);
+      return elemento; 
+}
+
+a();
+*/
 };
+
