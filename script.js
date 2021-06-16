8a3dcc23732faeffb0b43520e1d81ea9484aafc0
Ramon Vitor <vitorramon@hotmail.com>
2021-06-16 17:19:20 -0300

    Resolvido requisito 2

---
+++ script.js
@@ -0,0 +1,13 @@
+// function verificaLogin() {
+//   const login = document.getElementsByClassName('login');
+//   const senha = document.getElementsByClassName('senha');
+//   const logar = document.getElementsByClassName('btn-logar');
+//   logar.addEventListener('click', function() {
+//     if (login.value !== 'tryber@teste.com' && senha.value !== '123456') {
+//       alert('Login ou senha inválidos.');
+//     } else {
+//       alert('Olá, Tryber!')
+//     }
+//   })
+// }
+// verificaLogin();
