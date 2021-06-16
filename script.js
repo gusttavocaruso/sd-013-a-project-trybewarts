f74b12843a3bdb7e6fb9938b135d8956d6315e73
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-16 17:17:27 -0300

    resolvendo erros de lint

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const login = document.getElementById('login');
 const password = document.getElementById('password');
 
-function validar(){
+const validar = () => {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
-  return alert('Olá, Tryber!');
+    return alert('Olá, Tryber!');
   }
   return alert('Login ou senha inválidos.');
-}
+};
+
+validar();
