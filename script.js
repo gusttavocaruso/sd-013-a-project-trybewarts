03c8a8bf8d2bc48adc2baf180fda2d5df3d51d79
Rafael Victor <rafavictorgs@gmail.com>
2021-06-16 17:51:36 -0300

    Corrige erros do Lint

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const getBtnLogin = document.querySelector('#btn-login');
 
-getBtnLogin.addEventListener('click', function() {
-  let getInputLogin = document.querySelector
-  ('#input-login');
-  let getInputSenha = document.querySelector('#input-senha');
+getBtnLogin.addEventListener('click', () => {
+  const getInputLogin = document.querySelector('#input-login');
+  const getInputSenha = document.querySelector('#input-senha');
   if (getInputLogin.value !== 'tryber@teste.com' || getInputSenha.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-})
+});
