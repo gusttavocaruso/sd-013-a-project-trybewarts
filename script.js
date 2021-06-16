a01b162fa77abb8a7fbddc9dbd154db9d8ae7e67
Matheus <matheus.castro.macedo.xi@gmail.com>
2021-06-16 18:04:27 -0300

    Cosertado linit 3

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function verificaLogin() {
-   const login = document.querySelector('.login');
-   const senha = document.querySelector('.senha');
-   const logar = document.querySelector('.btn-logar');
-    logar.addEventListener('click', function() {
-     if (login.value != 'tryber@teste.com' || senha.value != '123456') {
-       alert('Login ou senha inválidos.');
-     } else {
-       alert('Olá, Tryber!')
+  const login = document.querySelector('.login');
+  const senha = document.querySelector('.senha');
+  const logar = document.querySelector('.btn-logar');
+    logar.addEventListener('click', (event) => {
+     if (login.value !== 'tryber@teste.com' || senha.value !== '123456') {
+      alert('Login ou senha inválidos.');
+      } else {
+       alert('Olá, Tryber!');
      }
-   })
- }
- verificaLogin(); 
+  });
+}verificaLogin(); 
