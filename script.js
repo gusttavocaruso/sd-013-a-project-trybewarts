73d4377bba475b0ae81b403a70c28ce8fce84012
Caê <eduardokae01@hotmail.com>
2021-06-16 19:35:06 -0300

    arrumando requisito 3

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('password');
 
 botaoEntrar.addEventListener('click', () => {
-  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+  if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
     alert('Olá, Tryber!');
   }
-  alert('Login ou senha inválidos.');
 });
