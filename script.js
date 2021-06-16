eb28c5c5cef645850821e562b9fa3a4fe1f12aa1
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-16 16:38:28 -0300

    resolvendo requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
-
-const botaoLogar = document.querySelector('#logar');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
 
 function validar(){
-
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
+    return alert('Olá, Tryber!');
+  }
+  return alert('Login ou senha inválidos.');
 }
