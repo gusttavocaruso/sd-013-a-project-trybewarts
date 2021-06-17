4634d1674d8d9bf8e969f97f309218a1c8b1331a
icaroMelo <exemployt@gmail.com>
2021-06-17 15:31:48 -0300

    Realizo correções e Requisitos 3,4,5,6,7

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ function req3() {
   const login = document.querySelector("#login");
   const senha = document.querySelector("#senha");
 
-    if(login.value !== "tryber@teste.com" || senha.value !== "123456") {
-      alert("Login ou senha inválidos.");
-    }
     if(login.value === "tryber@teste.com" && senha.value === "123456"){
       alert("Olá, Tryber!");
+    } else {
+      alert("Login ou senha inválidos.");
     }
    
-
   }
 
 addEventListener('submit',req3);
