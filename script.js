9db8f951d1626e1cbc0fcbcf840f7ce66fc40bc1
Danilo Uehara <daniloyuno@hotmail.com>
2021-06-16 23:18:48 -0300

    corrigir erros lint

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const login = document.querySelector("input[name=login");
-const senha = document.querySelector("input[name=senha");
-const pressButton = document.querySelector("button[name=button");
-let submitBtn = document.getElementById("submit-btn");
-const agreement = document.getElementById("agreement");
+const login = document.querySelector('input[name=login');
+const senha = document.querySelector('input[name=senha');
+const pressButton = document.querySelector('button[name=button');
+const submitBtn = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
 submitBtn.disable = true;
 
-pressButton.addEventListener("click", () => {
-  if (login.value === "tryber@teste.com" && senha.value === "123456") {
-    alert("Olá, Tryber!");
+pressButton.addEventListener('click', () => {
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Login ou senha inválidos.");
+    alert('Login ou senha inválidos.');
   }
 });
 
 function test() {
   if (agreement.checked) {
-    console.log("sim");
+    console.log('sim');
     submitBtn.disable = false;
   } else {
-    console.log("não");
+    console.log('não');
     submitBtn.disable = true;
   }
 }
-agreement.addEventListener("click", test);
+agreement.addEventListener('click', test);
