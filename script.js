c03e7667fb70f36e3ddfc3e5b2ba098357fc9c4e
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-06-17 01:47:33 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function verificaLogin() {
 formLogin.addEventListener('submit', verificaLogin);
 
 const checkboxConcorda = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
 function checkConcorda(event) {
-  const submitBtn = document.querySelector('#submit-btn');
   if (event.srcElement.checked === true) {
     submitBtn.disabled = false;
   } else {
@@ -24,7 +24,48 @@ checkboxConcorda.addEventListener('click', checkConcorda);
 const textarea = document.querySelector('#textarea');
 function contadorCaracter(event) {
   const counter = document.querySelector('#counter');
-  let contador = 500;
+  const contador = 500;
   counter.innerHTML = contador - event.target.value.length;
 }
 textarea.addEventListener('keyup', contadorCaracter);
+
+const evaluationForm = document.querySelector('#evaluation-form');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const inputSelect = document.querySelector('#house');
+const radioAvalaia = document.querySelectorAll('.radioAvalia');
+const radioFamilia = document.querySelectorAll('.radioFamilia');
+const checkbosAprender = document.querySelectorAll('.checkbosAprender');
+const textArea = document.querySelector('#textarea');
+
+function loopRadios(radioList) {
+  for (let index = 0; index < radioList.length; index += 1) {
+    if (radioList[index].checked === true) {
+      return radioList[index].value;
+    }
+  }
+}
+
+function loopCheckBox(radioList) {
+  let listaMaterias = '';
+  for (let index = 0; index < radioList.length; index += 1) {
+    if (radioList[index].checked === true) {
+      listaMaterias += `${radioList[index].value}, `;
+    }
+  }
+  return listaMaterias;
+}
+
+function salveForm(event) {
+  event.preventDefault();
+  const option = inputSelect.options[inputSelect.selectedIndex];
+  evaluationForm.innerHTML = `<p>Nome: ${inputName.value} ${inputLastName.value}</p>`;
+  evaluationForm.innerHTML += `<p>Email: ${inputEmail.value}</p>`;
+  evaluationForm.innerHTML += `<p>Casa: ${option.value}</p>`;
+  evaluationForm.innerHTML += `<p>Família: ${loopRadios(radioFamilia)}</p>`;
+  evaluationForm.innerHTML += `<p>Matérias: ${loopCheckBox(checkbosAprender).slice(0, -2)}</p>`;
+  evaluationForm.innerHTML += `<p>Avaliação: ${loopRadios(radioAvalaia)}</p>`;
+  evaluationForm.innerHTML += `<p>Observações: ${textArea.value}</p>`;
+}
+evaluationForm.addEventListener('submit', salveForm);
