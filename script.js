8a42c3340f63ee90d976822a852f8e1c5a26ef2a
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-17 18:52:10 -0300

    Adiciona requisito 3

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const loginButton = document.querySelector('#login-button');
-const loginInput = document.querySelector('#login-input');
-const passwordInput = document.querySelector('#password-input');
+const loginButton = document.getElementById('login-button');
+const loginInput = document.getElementById('login-input');
+const passwordInput = document.getElementById('password-input');
+const disableButton = document.getElementById('submit-btn').disabled;
+const agreement = document.getElementById('agreement');
 
 
 function loginAlert() {
-  if (loginInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456789'){
-    alert('Login ou senha inválidos');
+  if ((loginInput.value !== 'tryber@teste.com') || (passwordInput.value !== '123456')){
+    alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
 }
 
 loginButton.addEventListener('click', loginAlert);
+
+function disableBtn () {
+  if () {
+    
+  }
+}
+
+disableBtn();
 
 
