9901b45fe16164247f5dd0350267d29921d367cc
Danilo Uehara <daniloyuno@hotmail.com>
2021-06-16 23:26:10 -0300

    corrigir erros lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const senha = document.querySelector('input[name=senha');
 const pressButton = document.querySelector('button[name=button');
 const submitBtn = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
-submitBtn.disable = true;
+submitBtn.disabled= true;
 
 pressButton.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -16,10 +16,10 @@ pressButton.addEventListener('click', () => {
 function test() {
   if (agreement.checked) {
     console.log('sim');
-    submitBtn.disable = false;
+    submitBtn.disabled = false;
   } else {
     console.log('não');
-    submitBtn.disable = true;
+    submitBtn.disabled = true;
   }
 }
 agreement.addEventListener('click', test);
