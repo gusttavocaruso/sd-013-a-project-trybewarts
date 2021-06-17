1ee047b5fea2794d1e469248e3321524805438e7
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-06-17 15:48:20 -0300

    executando o lint

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 /* Variaveis */
-let btnLogin = document.getElementById("btnlogin");
-let inputLogin = document.getElementById("login")
-let inputSenha = document.getElementById("senha")
+const btnLogin = document.getElementById('btnlogin');
+const inputLogin = document.getElementById('login');
+const inputSenha = document.getElementById('senha');
 
 /* função validar login */
-btnLogin.addEventListener("click", () => {
-  if(inputLogin.value === 'tryber@teste.com' || inputSenha.value === '123456'){
-    alert("Olá, Tryber!");
-    }else {
-      alert("Login ou senha inválidos.");
-    }
-});
+btnLogin.addEventListener('click', () => {
+  if (inputLogin.value === 'tryber@teste.com' || inputSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }});
