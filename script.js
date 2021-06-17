5b9b00e27237454596952c5a0395ed6cf86b359d
icaroMelo <exemployt@gmail.com>
2021-06-17 18:20:06 -0300

    realizo requisito 14

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
 const ok = document.querySelector("#login");
-
 function req3() {
-
   const login = document.querySelector("#login");
   const senha = document.querySelector("#senha");
-
-    if(login.value === "tryber@teste.com" && senha.value === "123456"){
+    if (login.value === "tryber@teste.com" && senha.value === "123456") {
       alert("Olá, Tryber!");
     } else {
       alert("Login ou senha inválidos.");
     }
-   
   }
-
 addEventListener('submit',req3);
-
-// function req14 () { 
-//   for (let i = 0; i < 10; i +=1) {
-//     let inputs = document.querySelector('.req14');
-
-//     let checkbox = document.createElement('input');
-//     checkbox.value = [i];
-//     checkbox.rate = 'rate';
-//     checkbox.type = 'radio';
-//     inputs.appendChild('checkbox');
-
-//   }
-// }
-
-// req14();
-
