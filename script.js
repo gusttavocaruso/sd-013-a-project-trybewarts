da7a7240469a3e9022e3f1621617953c567b2f68
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-06-16 21:49:02 -0300

    resolve requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const valid_login = 'tryber@teste.com';
-const valid_password = '123456';
+/* Variáveis */
+const validLogin = 'tryber@teste.com';
+const validPassword = '123456';
 
 /* Elementos */
 const inputLogin = document.getElementById('input-login');
 const inputPassword = document.getElementById('input-password');
+const inputAgree = document.getElementById('agreement');
 
 /* Botões */
 const loginBtn = document.getElementById('login-btn');
+const submitBtn = document.getElementById('submit-btn');
 
+/* Função para autenticar login */
 function authLogin() {
-  if (inputLogin.value !== valid_login || inputPassword.value !== valid_password) {
+  if (inputLogin.value !== validLogin || inputPassword.value !== validPassword) {
     alert('Login ou senha inválidos.');
-  }
-  else {
+  } else {
     alert('Olá, Tryber!');
   }
 }
 loginBtn.addEventListener('click', authLogin);
+
+/* Função para habilitar envio de formulário */
+function enableSubmit() {
+  if (inputAgree.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+inputAgree.addEventListener('change', enableSubmit);
