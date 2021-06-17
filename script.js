1e16f5fc79af0a6ed89671cdabc24dca7bbfccde
Jackson Santana <onjacksm@gmail.com>
2021-06-17 04:17:08 -0300

    Resolve requisitos de 8 a 14 e adiciona comentários

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// Validação login
-// function validateLogin() { 
-//   let x = document.forms["trybeForm"]["name"].value; 
-//   let y = document.forms["trybeForm"]["pass"].value;
-//   if (x === 'tryber@teste.com' && y === "123456") { 
-//       alert("Olá, Tryber!")
-//     }
-//     else {
-//       alert("Login ou senha inválidos.");
-//     }
-// } 
-// document.getElementById("buttonId").addEventListener("click", validateLogin);
-
-function validateLogin(event) { 
-  let x = document.forms["trybeForm"]["name"].value + document.forms["trybeForm"]["pass"].value;
-  console.log(document.getElementById("buttonId"));
-  if (x === "tryber@teste.com123456") { 
-    alert("Olá, Tryber!"); 
-  } else { 
-    alert("Login ou senha inválidos."); 
+// Faz a validação do Login
+function validateLogin() {
+  const x = document.forms['trybeForm']['name'].value + document.forms['trybeForm']['pass'].value;
+  if (x === 'tryber@teste.com123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
-  event.preventDefault(); 
-} 
-document.getElementById("buttonId").addEventListener("click", validateLogin);
+}
+document.getElementById('buttonId').addEventListener('click', validateLogin);
