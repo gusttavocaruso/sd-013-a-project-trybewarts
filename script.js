bf09c9b3760f5e5bca07dfeaa3d2eae06f782321
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-06-17 09:57:11 -0300

    Requisito 3 concluido

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const desafio3 = () => {
+  let login = document.querySelector('#login');
+  let senha = document.querySelector('#senha');
+
+    if((login.value !== 'tryber@teste.com' ) && (senha.value !== '123456')) {
+      alert('Login ou senha inválidos.');
+    } else {
+      alert('Olá, Tryber!');
+    }
+}
+
+
+
+window.onload = () => {
+  let btnLogin = document.querySelector('#btn-login');
+  btnLogin.addEventListener('click',desafio3);
+}
+
