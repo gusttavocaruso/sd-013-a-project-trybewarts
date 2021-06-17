de0523c55ea3eba157011be09162786c07769ac0
thomasravache <thomasravache31@gmail.com>
2021-06-17 14:05:09 -0300

    Tentando resolver o requisito 21

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const txtLogin = document.querySelector('#login');
 const txtSenha = document.querySelector('#senha');
 const btnEnviar = document.querySelector('#submit-btn');
 const agreement = document.querySelector('#agreement');
-const comment = document.querySelector('#textarea');
+const comentario = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
+const txtNome = document.querySelector('#input-name');
+const txtSobrenome = document.querySelector('#input-lastname');
+const txtEmail = document.querySelector('#input-email');
+const listCasa = document.querySelectorAll('#house option');
+const rdbSatisfacao = document.querySelectorAll('.evaluation-form-radios input:checked');
+
+const formulario = document.querySelector('#evaluation-form');
+const imgTrybewarts = document.querySelector('#trybewarts-forms-logo');
+
 
 counter.innerHTML = 500;
 const qtdCharacters = parseInt(counter.textContent, 10);
@@ -31,8 +40,35 @@ agreement.addEventListener('click', function validateEnviar(event) {
   return this;
 });
 
-comment.addEventListener('keyup', function commentCount() {
-  counter.innerHTML = qtdCharacters - comment.value.length;
+comentario.addEventListener('keyup', function comentarioCount() {
+  counter.innerHTML = qtdCharacters - comentario.value.length;
 
   return this;
+});
+
+formulario.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
+
+btnEnviar.addEventListener('click', function () {
+  formulario.style.display = 'none';
+  imgTrybewarts.style.display = 'none';
+  const rdbFamilia = document.querySelector('.evaluation-form-radio input:checked');
+  const chkConteudo = document.querySelectorAll('.evaluation-form-checks input:checked');
+
+
+  document.querySelector('#resultado-nome').innerHTML = txtNome.value;
+  document.querySelector('#resultado-email').innerHTML = txtEmail.value;
+  document.querySelector('#resultado-casa').innerHTML = listCasa.value;
+  document.querySelector('#resultado-familia').innerHTML = rdbFamilia.value;
+
+  let string = '';
+  for (let index = 0; index < chkConteudo.length; index += 1) {
+    string += chkConteudo[index].value + ',';
+  }
+  document.querySelector('#resultado-materias').innerHTML = string;
+
+  // formulario.remove();
+  // imgTrybewarts.remove();
+
 });
