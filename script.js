2a70fb691a942f8eab7260a8ec87654e58a105eb
suco <francalucas937@gmail.com>
2021-06-17 11:03:45 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-<<<<<<< HEAD
+
 const btnForm = document.getElementById('btn');
 const loginBtn = document.getElementById('loginBtn');
 const senhaBtn = document.getElementById('senhaBtn');
 const email = 'tryber@teste.com';
 const senha = '123456';
-btnForm.addEventListener('click', () => {
+btnForm.addEventListener('click', Eventclick)
+
+function Eventclick() {
   if ((loginBtn.value !== email) || (senhaBtn.value !== senha)) {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-});
+};
 
 const botao = document.querySelector("#submit-btn");
 const checkBox = document.querySelector("#agreement");
