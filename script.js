64773e93f1c520b7d2a4a142e332c424f084e798
CarlosLima <carlos.augustolimaf@gmail.com>
2021-06-17 13:29:02 -0300

    Adicionao requisito 20

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const buttonHeader = document.getElementById('button-submit-header');
 const inputLogin = document.getElementById('login-input');
 const inputSenha = document.getElementById('senha-input');
-buttonHeader.addEventListener('click', verificarFormHeader);
 
 function verificarFormHeader() {
-    const login = inputLogin.value;
-    const senha = inputSenha.value;
-    if ((senha === "123456") && (login === "tryber@teste.com")) {
-      alert("Olá, Tryber!");
-    } else {
-      alert("Login ou senha inválidos.");
-    }
+  const login = inputLogin.value;
+  const senha = inputSenha.value;
+  if ((senha === '123456') && (login === 'tryber@teste.com')) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 }
 
-document.getElementById("submit-btn").disabled = true;
-document.getElementById("agreement").addEventListener("input", function(event){
-    var conteudo = document.getElementById("agreement").value;
-    if (conteudo !== null && conteudo !== '') {
-      document.getElementById("submit-btn").disabled = false;
-    } else {
-      document.getElementById("submit-btn").disabled = true;
-    }
-});
+buttonHeader.addEventListener('click', verificarFormHeader);
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.disabled = true;
+
+function verificaCheck() {
+  const conteudo = document.getElementById('agreement').value;
+  if (conteudo !== null && conteudo !== '') {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+document.getElementById('agreement').addEventListener('input', verificaCheck);
+
+const textArea = document.getElementById('textarea');
+const cont = document.getElementById('counter');
+
+function contadorCaractere() {
+  const value = parseInt(textArea.value.length, 10);
+  let caracteresRestantes = 500;
+  const caracteresDigitados = value;
+  caracteresRestantes -= caracteresDigitados;
+  cont.innerText = caracteresRestantes;
+}
+
+textArea.addEventListener('keydown', contadorCaractere);
