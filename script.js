949238cdf2afef58531f9751335822bedc0441b2
Emídio Rebouças <emidioreb@gmail.com>
2021-06-17 12:02:36 -0300

    realização do requisito 3 no JS

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function statusButton() {
   }
 }
 checkedStatus.addEventListener('click', statusButton);
+
+// Requito 3
+function submitButton(){
+  const buttonSubmit =  document.getElementById('buttonSubmit');
+  buttonSubmit.addEventListener('click', () => {
+  const user = document.getElementById('user');
+  const password = document.getElementById('password');
+  if (user.value === 'tryber@teste.com' && password.value === '123456'){
+    alert('Olá, Tryber!');
+  }
+  if(user.value !== 'tryber@teste.com' || password.value !== '123456'){
+    alert('Login ou senha inválidos.');
+  }
+  });
+}
+submitButton();
