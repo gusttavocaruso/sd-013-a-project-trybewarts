1b35db2d7329c5214fa196ec497745d503519a11
Caê <eduardokae01@hotmail.com>
2021-06-17 13:56:26 -0300

    arruma requisito 3, resolve requisito 18 e 19

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const botaoEntrar = document.querySelector('.botao-entrar');
-const inputEmail = document.getElementById('email');
-const inputSenha = document.getElementById('password');
+// requisito 3
+const btnLogin = document.getElementById('btn-login');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
 
-botaoEntrar.addEventListener('click', () => {
-  if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
-    alert('Login ou senha inválidos.');
-  } else {
+btnLogin.addEventListener('click', () => {
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  };
+});
+
+// requisito 18
+const checkbox = document.querySelector('#agreement');
+const botaoSubmit = document.querySelector('#submit-btn');
+
+checkbox.addEventListener('click', () => {
+  if (checkbox.checked) {
+    botaoSubmit.removeAttribute('disabled');
   }
 });
