0b7abf1990eb76c0c442acb7b8c47020dc608f11
Gustavo Mauricio <gmauricio2072gmail.com>
2021-06-17 11:12:47 -0300

    Exercício 20

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const botao = document.querySelector("#submit-btn");
-const checkBox = document.querySelector("#agreement");
+const botao = document.querySelector('#submit-btn');
+const checkBox = document.querySelector('#agreement');
 
 function oi () {
   if (checkBox.checked) {
@@ -8,3 +8,24 @@ function oi () {
     botao.disabled = true;
   }
 }
+
+// CONTADOR TEXTAREA
+function  () {
+  const areaText = document.querySelector('#story');
+  const counterText = document.querySelector('#counter');                                                                            
+  addEventListener(areaText, 'focus', updateCounter);
+  addEventListener(areaText,'input', updateCounter);
+
+  addEventListener(areaText,'blur', function() {
+    if(areaText.nodeValue.lengh <= 500){
+      counterText.className = 'hide'
+    }
+  });
+
+  function updateCounter(e) {
+    let target = e.target || e.srcElement;
+    let contador = 500 - target.value.length;
+    if(count < 0)
+  }
+}
+
