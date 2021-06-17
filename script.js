5327d8f79a4849b9547201d0359fc543b68223ef
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-17 12:08:21 -0300

    add 18th requirement solved

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // função responsavel pelo requisito numero 3
 const logBtn = document.getElementById('log-btn');
+const agreeCheck = document.querySelector('#agreement');
+const sendButton = document.querySelector('#submit-btn');
 
 logBtn.addEventListener('click', () => {
   const login = document.getElementById('login').value;
@@ -8,5 +10,14 @@ logBtn.addEventListener('click', () => {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+// Habilita/Desabilita Botão 
+agreeCheck.addEventListener('click', () => {
+  if (agreeCheck.checked === true) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
   }
 });
