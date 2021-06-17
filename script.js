12fcbb6e634e763bfe1512bb5455503cb98143f8
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-06-16 21:22:23 -0300

    a

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-  const buttonLogin = document.getElementById('login-button');
+const buttonLogin = document.getElementById('login-button');
 
-  buttonLogin.addEventListener('click', () => {
-    const inputlogin = document.getElementById('login-input').value;
-    const inputPass = document.getElementById('pass-input').value;
-    if (inputlogin !== 'tryber@teste.com' || inputPass !== '123456') {
-      return alert('Login ou senha inválidos.');
-    }
-      return alert('Olá, Tryber!');
-  })
+buttonLogin.addEventListener('click', () => {
+  const inputlogin = document.getElementById('login-input').value;
+  const inputPass = document.getElementById('pass-input').value;
+  if (inputlogin !== 'tryber@teste.com' || inputPass !== '123456') {
+    return alert('Login ou senha inválidos.');
+  }
+    return alert('Olá, Tryber!');
+})
