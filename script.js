4c3a9100384b170070b0bb5690e29f0deec3cc63
Andrey Gois <gois.andrey@hotmail.com>
2021-06-17 19:16:10 -0300

    Adiciona questoes que faltavam

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginButton = document.getElementById('login-button');
+const loginInput = document.getElementById('login-input');
+const passwordInput = document.getElementById('password-input');
+const disableButton = document.getElementById('submit-btn').disabled;
+const agreement = document.getElementById('agreement');
+
+function loginAlert() {
+  if ((loginInput.value !== 'tryber@teste.com') || (passwordInput.value !== '123456')) {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
