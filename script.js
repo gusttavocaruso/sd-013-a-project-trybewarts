5233ecd849ef6b8b4b9d70f3beaf15671c661efb
Danilo Uehara <daniloyuno@hotmail.com>
2021-06-16 22:32:11 -0300

    add 14-15-16-17-19

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-window.onload = function () {
+window.onload = function(){
+
+
   const login = document.querySelector('input[name=login');
   const senha = document.querySelector('input[name=senha');
   const pressButton = document.querySelector('button[name=button');
+  const buttonEnviar = document.getElementById('submit-btn');
+  const agreement = document.getElementById('agreement');
+
   pressButton.addEventListener('click', () => {
-    if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    if ((login.value === 'tryber@teste.com') && (senha.value === '123456')) {
       alert('Olá, Tryber!');
     } else {
       alert('Login ou senha inválidos.');
     }
   });
+
+
+function desativa (){
+  
+  buttonEnviar.disable = true;
+}
+function habilitar (){
+  buttonEnviar.disable = false;
+
+}
+    
+ /* const checkbox = document.querySelector('input[name=aceita'); */
+function checked (){
+  agreement.addEventListener('change',()=>{
+    if (agreement.checked){
+      habilitar();    
+    }    
+    else{
+        desativa();
+    }
+  });
+}
+
+checked();
+}
+  /*
   const ajuda = document.getElementsByClassName('satisfação');
 
 
-/*
+
 function a(){  
   for (let aux=0; aux < 10; aux+=1){
     let label = criar('Label');
@@ -33,5 +64,3 @@ function a(){
 
 a();
 */
-};
-
