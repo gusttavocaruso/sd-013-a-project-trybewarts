c5677305989ff83c067db06d3b57ef13495245be
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-06-17 12:46:51 -0300

    Requisitos 13, 14 e 15

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function check() {
 
 const botao = document.getElementById('botao');
 botao.addEventListener('click', check);
+
+const radioButtons = document.getElementById('radioButtons');
+for (let i = 1; i <= 10; i += 1) {
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  label.setAttribute('for', 'radio-buttons[i]');
+  label.innerHTML = i;
+  input.name = 'rate';
+  input.type = 'radio';
+  input.value = i;
+  label.appendChild(input);
+  radioButtons.appendChild(label);
+}
