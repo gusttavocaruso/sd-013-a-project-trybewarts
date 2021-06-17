9e85636761fe3ad4b89889d9a0690f8eecce3fa6
Matheus <matheushtc25@gmail.com>
2021-06-17 11:28:33 -0300

    Resolvendo os erros do linter

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const buttonHeader = document.getElementById('button-submit-header');
 const inputLogin = document.getElementById('login-input');
 const inputSenha = document.getElementById('senha-input');
-buttonHeader.addEventListener('click', verificarFormHeader);
 
 function verificarFormHeader() {
-    const login = inputLogin.value;
-    const senha = inputSenha.value;
-    if ((senha === "123456") && (login === "tryber@teste.com")) {
-      alert("Olá, Tryber!");
-    } else {
-      alert("Login ou senha inválidos.");
-    }
+  const login = inputLogin.value;
+  const senha = inputSenha.value;
+  if ((senha === '123456') && (login === 'tryber@teste.com')) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 }
 
-document.getElementById("submit-btn").disabled = true;
-document.getElementById("agreement").addEventListener("input", function(event){
-    var conteudo = document.getElementById("agreement").value;
-    if (conteudo !== null && conteudo !== '') {
-      document.getElementById("submit-btn").disabled = false;
-    } else {
-      document.getElementById("submit-btn").disabled = true;
-    }
-});
+buttonHeader.addEventListener('click', verificarFormHeader);
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.disabled = true;
+
+function verificaCheck() {
+  const conteudo = document.getElementById('agreement').value;
+  if (conteudo !== null && conteudo !== '') {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+document.getElementById('agreement').addEventListener('input', verificaCheck);
