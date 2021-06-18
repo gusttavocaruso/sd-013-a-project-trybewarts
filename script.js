d2fb439ab703256c93bb79e08a3e30ddb36e7bee
Felipe Lima <felipe_lima10@icloud.com>
2021-06-17 23:51:14 -0300

    Adicionando responsividade

--- script.js
+++ script.js
@@ -4,23 +4,36 @@ const inputEmail = document.querySelector('#email');
 const inputPassword = document.querySelector('#password');
 
 formButton.addEventListener('click', () => {
-  if (inputEmail.value !== 'tryber@teste.com' && inputPassword.value !== '123456') {
-    alert('Login ou senha inválidos.');
-  } else {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
     alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
 });
 
 // Faça com que o botão 'Enviar' seja habilitado somente após a checkbox do requisito 16 ser selecionada (requesito 18)
 const sendButtom = document.getElementById('submit-btn');
 const checkAgree = document.getElementById('agreement');
-sendButtom.checked = true;
-console.log(sendButtom.toggleAttribute);
-function sendForm() {
+sendButtom.disabled = true;
+checkAgree.addEventListener('click', () => {
   if (checkAgree.checked === true) {
     sendButtom.disabled = false;
   } else {
     sendButtom.disabled = true;
   }
+});
+
+// Requisito 14
+const radioDiv = document.querySelector('.radio-buttons-rate');
+for (let index = 1; index <= 10; index += 1) {
+  const label = document.createElement('label');
+  const radio = document.createElement('input');
+  label.setAttribute('for', `radio-button-${index}`);
+  label.innerHTML = index;
+  radio.type = 'radio';
+  radio.name = 'rate';
+  radio.value = index;
+  radio.id = `radio-button-${index}`;
+  label.appendChild(radio);
+  radioDiv.appendChild(label);
 }
-checkAgree.addEventListener('click', sendForm);
