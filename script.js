2e9c828d49d0fae9ce450db19dff3b7448b359b6
Filipe <filipecandido123@hotmail.com>
2021-06-17 22:49:56 -0300

    requisito 21

--- script.js
+++ script.js
@@ -21,3 +21,49 @@ checker.onchange = function habilita() {
     sendBtn.disabled = true;
   }
 };
+
+function getFamily() {
+  const family = document.getElementsByName('family');
+  for (const key in family) {
+    if (family[key].checked) {
+      return family[key].value
+    }
+  }
+}
+
+function getSubjected() {
+  const subjects = document.getElementsByClassName('subject');
+  let selected= '';
+  for (const key in subjects) {
+    if (subjects[key.checked]) {
+      selected += subjects[key].value
+    }
+  } 
+  return selected;
+}
+
+function getAvaliation() {
+  const avaliacoes = document.getElementsByName('rate');
+  for (const key in avaliacoes) {
+    if (avaliacoes[key].checked) {
+      return avaliacoes[key].value
+    }
+  }
+}
+
+const name
+
+function preencheForm() {
+  const familiaSelecionada = getFamily();
+  const subObjetoSelecionado = getSubjected();
+  const avaliacaoDoUsuario = getAvaliation();
+  const form = document.getElementsByTagName('form')[1];
+  form.innerHTML = '';
+  form.innerHTML += `Nome: ${inputName.value} ${inputLastN.value} <br>`;
+  form.innerHTML += `Email: ${inputEmail.value} <br>`;
+  form.innerHTML += `Casa: ${house.value} <br>`;
+  form.innerHTML += `Família: ${familiaSelecionada} <br>`;
+  form.innerHTML += `Matérias: ${subObjetoSelecionado} <br>`;
+  form.innerHTML += `Avaliação: ${avaliacaoDoUsuario} <br>`;
+  form.innerHTML += `Observações: ${textArea.value}`;
+}
