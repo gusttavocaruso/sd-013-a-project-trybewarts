a2ad6c714a66df31099680786ace718e414090d0
turtore <brunosap@gmail.com>
2021-06-17 21:15:21 -0300

    finalizados requisitos bonus, erros no lint

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoLogin = document.querySelector('#botaoLogin');
 const checkBox = document.querySelector('#agreement');
 const campoTexto = document.querySelector('#textarea');
 const botaoSubmit = document.querySelector('#submit-btn');
-//const form = document.querySelector('#submit-btn');
+// const form = document.querySelector('#submit-btn');
 
 function verificaLogin() {
   const campoUser = document.querySelector('#email').value;
@@ -39,23 +39,28 @@ const house = document.querySelector('#house');
 function apareceInfo() {
   const familia = document.querySelector('input[name=family]:checked');
   const materias = document.querySelectorAll('.subject:checked');
-    
+  const notaMarcada = document.querySelector('input[name=rate]:checked');
+  const textoArea = document.querySelector('#textarea');
+
   document.querySelector('#evaluation-form').remove();
   document.querySelector('#trybewarts-forms-logo').remove();
+
   const main = document.querySelector('main');
   main.id = 'evaluation-form';
   const section = document.createElement('section');
-  
-  let tempMaterias = ''
-  for (let i = 0; i < materias.length; i+= 1){
-    tempMaterias += `${materias[i].value}, `
+
+  let tempMaterias = '';
+  for (let i = 0; i < materias.length; i += 1) {
+    tempMaterias += `${materias[i].value}, `;
   }
   const p1 = document.createElement('p');
   const p2 = document.createElement('p');
   const p3 = document.createElement('p');
   const p4 = document.createElement('p');
   const p5 = document.createElement('p');
-  
+  const p6 = document.createElement('p');
+  const p7 = document.createElement('p');
+
   p1.innerHTML = `Nome: ${nome.value} ${lastName.value}`;
   section.appendChild(p1);
   p2.innerHTML = `Email: ${email.value}`;
@@ -63,15 +68,16 @@ function apareceInfo() {
   p3.innerHTML = `Casa: ${house.value}`;
   section.appendChild(p3);
   p4.innerHTML = `Família: ${familia.value}`;
-  section.appendChild(p4); 
-
+  section.appendChild(p4);
   p5.innerHTML = `Matérias: ${tempMaterias}`;
-  section.appendChild(p5); 
-
+  section.appendChild(p5);
+  p6.innerHTML = `Avaliação: ${notaMarcada.value}`;
+  section.appendChild(p6);
+  p7.innerHTML = `Observações: ${textoArea.value}`;
+  section.appendChild(p7);
 
   main.appendChild(section);
 }
-  
 
 botaoLogin.addEventListener('click', verificaLogin);
 checkBox.addEventListener('change', habilitaBotao);
