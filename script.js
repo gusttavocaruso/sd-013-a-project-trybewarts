25c530e78a7724f9f861f5567101d9dd24920faa
luiz furtado <luizfurtadomkt@hotmail.com>
2021-06-18 09:46:03 -0300

    updates nas features

--- script.js
+++ script.js
@@ -1,6 +1,86 @@
 const enviarBtn = document.querySelector('#submit-btn');
 const aceitoText = document.querySelector('#label-infos');
 const aceitoCheck = document.querySelector('#agreement');
+const chars = document.querySelector('#counter');
+const texarea = document.querySelector('#textarea');
+
+new window.JustValidate('#evaluation-form', {
+  rules: {
+    nome: {
+      required: true,
+    },
+    sobrenome: {
+      required: true,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    casa: {
+      required: true,
+    },
+    radio: {
+      required: true,
+    },
+    radio2: {
+      required: true,
+    },
+    textarea: {
+      required: true,
+    },
+    agree: {
+      required: true,
+    },
+  },
+  messages: {
+    nome: {
+      required: 'Preencha este campo!',
+    },
+    sobrenome: {
+      required: 'Preencha este campo.',
+    },
+    email: {
+      required: 'Preencha este campo!',
+      email: 'Digite um email válido',
+    },
+    casa: {
+      required: 'Escolha uma casa.',
+    },
+    radio: {
+      required: 'Escolha uma família.',
+    },
+    radio2: {
+      required: 'Escolha uma nota.',
+    },
+    textarea: {
+      required: 'Deixe um comentário.',
+    },
+    agree: {
+      required: 'Concorde com o uso das informações.',
+    },
+  },
+
+  focusWrongField: true,
+
+  submitHandler(form, values, ajax) {
+    ajax({
+      url: 'https://just-validate-api.herokuapp.com/submit',
+      method: 'POST',
+      data: values,
+      async: true,
+      callback(response) {
+        alert(`Formulário enviado! \nResposta do servidor: ${response}`);
+      },
+      error(response) {
+        alert(`AJAX submit error! \nResposta do servidor:${response}`);
+      },
+    });
+  },
+
+  invalidFormCallback(errors) {
+    console.log(errors);
+  },
+});
 
 function handler() {
   const concordo = document.querySelectorAll('input[name="concordo"]:checked');
@@ -9,6 +89,12 @@ function handler() {
   } else {
     enviarBtn.disabled = true;
   }
+}
+
+function counter() {
+  console.log('teste');
+  const count = texarea.value.length;
+  chars.innerText = 500 - count;
 }
 
 const login = document.getElementById('input-login');
@@ -24,3 +110,5 @@ logar.addEventListener('click', () => {
 });
 aceitoCheck.addEventListener('click', handler);
 aceitoText.addEventListener('click', handler);
+texarea.addEventListener('keyup', counter);
+texarea.addEventListener('paste', counter);
