50b0d4baf77fb89608ae4c25bb77545bc1610603
turtore <brunosap@gmail.com>
2021-06-17 21:53:31 -0300

    ultimo update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const botaoLogin = document.querySelector('#botaoLogin');
 const checkBox = document.querySelector('#agreement');
 const campoTexto = document.querySelector('#textarea');
+const botaoSubmit = document.querySelector('#submit-btn');
 
 function verificaLogin() {
   const campoUser = document.querySelector('#email').value;
@@ -29,6 +30,33 @@ function contaDigitos() {
   counterConst.innerHTML = calculo;
 }
 
+const nome = document.querySelector('#input-name');
+const lastName = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+
+function apareceInfo() {
+  const familia = document.querySelector('input[name=family]:checked');
+  const materias = document.querySelectorAll('.subject:checked');
+  const notaMarcada = document.querySelector('input[name=rate]:checked');
+  const textoArea = document.querySelector('#textarea');
+  document.querySelector('#evaluation-form').remove();
+  document.querySelector('#trybewarts-forms-logo').remove();
+  const main = document.querySelector('main');
+  main.id = 'evaluation-form';
+  const section = document.createElement('section');
+  let tempMaterias = '';
+  for (let i = 0; i < materias.length; i += 1) {
+    tempMaterias += `${materias[i].value}, `;
+  }
+  section.innerHTML = `<p>Nome: ${nome.value} ${lastName.value}</p> <p>Email: ${email.value}</p>
+  <p>Casa: ${house.value}</p> <p>Família: ${familia.value}</p> <p>Matérias: ${tempMaterias}</p>
+  <p>Avaliação: ${notaMarcada.value}</p> <p>Observações: ${textoArea.value}</p>`;
+
+  main.appendChild(section);
+}
+
 botaoLogin.addEventListener('click', verificaLogin);
 checkBox.addEventListener('change', habilitaBotao);
 campoTexto.addEventListener('keyup', contaDigitos);
+botaoSubmit.addEventListener('click', apareceInfo);
