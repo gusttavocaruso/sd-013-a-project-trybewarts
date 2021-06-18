6cd0b1fdce47f8fe9a7ab1160b3c2f238e6e85dd
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-06-18 10:11:49 -0300

    Requisito 12 concluido

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 const desafio3 = () => {
   let login = document.querySelector('#login');
   let senha = document.querySelector('#senha');
-
+  let btnLogin = document.querySelector('#btn-login');
+  btnLogin.addEventListener('click',() => {
     if((login.value !== 'tryber@teste.com' ) && (senha.value !== '123456')) {
       alert('Login ou senha inválidos.');
     } else {
       alert('Olá, Tryber!');
     }
+  });
 }
 
-window.onload = () => {
-  let btnLogin = document.querySelector('#btn-login');
-  btnLogin.addEventListener('click',desafio3);
-}
+
+desafio3();
 
