332efddafd0d7a94ea2d4e01a287977d9a71b755
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-17 21:33:01 -0300

    resolvendo erros lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const login = document.getElementById('login');
 const password = document.getElementById('password');
 
 botaoLogar.addEventListener('click', () => {
-  if (login.value === 'tryber@teste.com' && password.value === '123456') {
+  if ((login.value === 'tryber@teste.com') && (password.value === '123456')) {
     return alert('Olá, Tryber!');
   }
   return alert('Login ou senha inválidos.');
@@ -12,19 +12,19 @@ botaoLogar.addEventListener('click', () => {
 const checker = document.getElementById('agreement');
 const sendbtn = document.getElementById('submit-btn');
 
-function ativar() {
+const ativar = () => {
   if (checker.checked) {
     sendbtn.disabled = false;
   } else {
     sendbtn.disabled = true;
   }
-}
+};
 
 ativar();
 
 const textArea = document.querySelector('#textarea');
 const counter = document.getElementById('counter');
 
-textArea.onkeyup = function piak() {
+textArea.onkeyup = function Math() {
   counter.innerHTML = `${500 - this.value.length} restantes`;
 };
