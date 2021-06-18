afa8cbdc2c52cd70d790b094ac52431b29f5b8c5
Matheus <matheus.castro.macedo.xi@gmail.com>
2021-06-18 19:36:18 -0300

    Requisito 18 consertado

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function verificaLogin() {
   });
 }
 verificaLogin();
+
+function habilitarBotao() {
+  const botao = document.querySelector('#submit-btn')
+  const checkBox = document.querySelector('#agreement')
+  botao.disabled = true 
+  checkBox.addEventListener('change', (event) => {
+    if(event.target.checked){
+      botao.disabled=false
+    } else {
+      botao.disabled=true
+    }
+  });
+}
+habilitarBotao();
