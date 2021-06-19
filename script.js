b5f5ee83a8f4ecafef8268145b4ee4cda1cd2d7a
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-06-19 06:37:28 -0300

    Corrigindo Lint

--- script.js
+++ script.js
@@ -3,59 +3,56 @@ const desafio3 = () => {
   const login = document.querySelector('#login');
   const senha = document.querySelector('#senha');
   const btnLogin = document.querySelector('#btn-login');
-  btnLogin.addEventListener('click',() => {
-    if((login.value !== 'tryber@teste.com' ) && (senha.value !== '123456')) {
+  btnLogin.addEventListener('click', () => {
+    if ((login.value !== 'tryber@teste.com') && (senha.value !== '123456')) {
       alert('Login ou senha inválidos.');
     } else {
       alert('Olá, Tryber!');
     }
   });
-}
+};
 desafio3();
 
 const desafio12 = () => {
   const divFamily = document.querySelector('#divFamily');
-  const languages = ['Frontend','Backend','FullStack'];
-
-  for(let i = 0; i < languages.length; i += 1) {
-    let divRadio = document.createElement('div');
+  const languages = ['Frontend', 'Backend', 'FullStack'];
+  for (let i = 0; i < languages.length; i += 1) {
+    const divRadio = document.createElement('div');
     const language = languages[i];
     const inputs = document.createElement('input');
-    inputs.setAttribute('type','radio');
-    inputs.setAttribute('value',language);
-    inputs.setAttribute('id',language);
-    inputs.setAttribute('name','family');
+    inputs.setAttribute('type', 'radio');
+    inputs.setAttribute('value', language);
+    inputs.setAttribute('id', language);
+    inputs.setAttribute('name', 'family');
     inputs.style.marginBottom = '10px';
     divRadio.appendChild(inputs);
-
     const labels = document.createElement('label');
-    labels.setAttribute('for',language);
+    labels.setAttribute('for', language);
     labels.innerText = language;
-    labels.setAttribute('class','languagesLabel');
     divRadio.appendChild(labels);
     divFamily.appendChild(divRadio);
   }
-}
+};
 desafio12();
 
 const desafio13 = () => {
   const checkbox = document.querySelector('#checkbox');
   const nomes = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
 
-  for(let i = 0; i < nomes.length; i += 1) {
+  for (let i = 0; i < nomes.length; i += 1) {
     const nome = nomes[i];
     const inputs = document.createElement('input');
     inputs.classList.add('subject');
     inputs.setAttribute('type', 'checkbox');
-    inputs.setAttribute('value',nome);
-    inputs.setAttribute('id',nome)
+    inputs.setAttribute('value', nome);
+    inputs.setAttribute('id', nome);
     checkbox.appendChild(inputs);
 
     const label = document.createElement('label');
-    label.setAttribute('for',nome);
+    label.setAttribute('for', nome);
     label.innerText = nome;
-    label.setAttribute('class','labels');
+    label.setAttribute('class', 'labels');
     checkbox.appendChild(label);
   }
-}
+};
 desafio13();
