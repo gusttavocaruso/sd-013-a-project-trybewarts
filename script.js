106e99a093067c693147294edf7018d87792edc9
ALINE <hoshinoaline@yahoo.com.br>
2021-06-19 10:28:31 -0300

    Arrumando Lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const senha = document.getElementById('senha');
 const buttonLogin = document.getElementById('buttonLogin');
 
 function validation() {
-  if ((login.value !== 'tryber@teste.com') && (senha.value !== '123456')) {
+  if ((login.value !== 'tryber@teste.com') || (senha.value !== '123456')) {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
