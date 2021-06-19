55705ea7699877755a6e0b0961e7db31007873e8
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-18 21:28:42 -0300

    finalizacao - requisito 21 - finalmentegit add .git add .

--- script.js
+++ script.js
@@ -12,8 +12,73 @@ botaoLogar.addEventListener('click', () => {
 });
 
 // const formEvaluation = document.querySelector('#evaluation-form');
+// debugger;
+
+function fnFillArrayRefSubjectSelected() {
+  // Materias selecionadas
+  const arrSubject = [];
+  const elAllSubject = document.querySelectorAll('.subject');
+  for (const subject of elAllSubject) {
+    if (subject.checked) {
+      arrSubject.push(` ${subject.value}`);
+    }
+  }
+  return arrSubject;
+}
+
+// ========================================================
+// Source: https://stackoverflow.com/questions/6533138/javascript-selected-radio/56091858
+function getCheckedRadioValue(name) {
+  const elements = document.getElementsByName(name);
+  for (let i = 0; i <= elements.length; i += 1) {
+    if (elements[i].checked) return elements[i].value;
+  }
+}
+// =========================================================
+
+function fnBuildReport() {
+  const strName = document.querySelector('#input-name').value;
+  const strLastName = document.querySelector('#input-lastname').value;
+  const strEmail = document.querySelector('#input-email').value;
+  const strHouse = document.querySelector('#house').value;
+  const strfamily = getCheckedRadioValue('family');
+  const arrSubjects = fnFillArrayRefSubjectSelected();
+  const intRate = getCheckedRadioValue('rate');
+  const strTextarea = document.querySelector('#textarea').value;
+  const objReport = {
+    name: `Nome: ${strName} ${strLastName}`,
+    email: `Email: ${strEmail}`,
+    house: `Casa: ${strHouse}`,
+    family: `Família: ${strfamily}`,
+    subject: `Matérias: ${Object.values(arrSubjects)}`,
+    rate: `Avaliação: ${intRate}`,
+    comment: `Observações: ${strTextarea}`,
+  };
+  const strReport = `${Object.values(objReport)}`;
+  return strReport;
+}
+
+function fnAppendReport() {
+  const elReport = document.createElement('div');
+  elReport.innerHTML = fnBuildReport();
+  const divReport = document.querySelector('#output-report');
+  divReport.appendChild(elReport);
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  fnAppendReport();
+}
 
 const btnSubmit = document.querySelector('#submit-btn');
+
+btnSubmit.addEventListener('click', handleSubmit);
+
+// for (let subject of elAllSubject) {
+//   if (subject.checked) {
+//     arrSubject.push(subject.checked.value);
+//   }
+// }
 
 const checkAgreement = document.querySelector('#agreement');
 
@@ -39,3 +104,23 @@ checkAgreement.addEventListener('click', enableSubmit);
 //     alert('O aceite referente ao uso dos dados é obrigatório');
 //   }
 // });
+
+// Deprecated ===================================================
+// Declaração e criacao da div que ficara abaixo do text area
+// let intMaxLengthComent = document.createElement('div');
+// intMaxLengthComent.id = 'counter';
+// intMaxLengthComent.innerHTML = '500';
+// Appendando abaixo do textarea
+// elemTextAreaForComent.append(intMaxLengthComent);
+// ==============================================================
+const elemTextAreaForComent = document.querySelector('#textarea');
+const elemToShowIntoTextArea = document.querySelector('#counter');
+
+elemTextAreaForComent.addEventListener('keyup', (evt) => {
+  const intMaxCaracteres = 500;
+  const intTotalCaracteresInTextArea = evt.target.value.length;
+  const intBalance = (intMaxCaracteres - intTotalCaracteresInTextArea);
+  // const strText = evt.target.innerHtml;
+  elemToShowIntoTextArea.innerHTML = intBalance;
+  // evt.target.innerHtml = `${strText} ${intBalance}`;
+});
