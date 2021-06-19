7e424a0d6cb90c3be6d406e37921a69644e410bc
Walace Nascimento <wlacens@hotmail.com>
2021-06-19 15:18:33 -0300

    Resolve o requisito 18

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function checkLogin() {
 }
 
 document.getElementsByClassName('btn-login')[0].addEventListener('click', checkLogin);
+
+function checkForm(event) {
+  const input = document.getElementById('agreement');
+  if (!input.checked) {
+    event.preventDefault();
+  }
+}
+document.getElementById('submit-btn').addEventListener('click', checkForm);
+
+function activeButton(event) {
+  const btn = document.getElementById('submit-btn');
+  if (event.target.checked) {
+    console.log('entrou');
+    btn.style.backgroundColor = 'indigo';
+    btn.disabled = false;
+    console.log(btn.disabled);
+  } else {
+    btn.style.backgroundColor = 'rgb(161, 159, 159)';
+    btn.disabled = true;
+    console.log(btn.disabled);
+  }
+}
+
+document.getElementById('agreement').addEventListener('change', activeButton);
