0800cd1d46759126bce7cece7f51b709e8844158
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-19 14:45:39 -0300

    Metade do requisito 21

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const agreementField = document.querySelector('#agreement');
 const submitButton = document.querySelector('#submit-btn');
 const textAreaField = document.querySelector('#textarea');
 const counterField = document.querySelector('#counter');
+const formField = document.querySelector('#evaluation-form');
+const firstNameInput = document.querySelector('#input-name');
+const lastNameInput = document.querySelector('#input-lastname');
+const emailInput = document.querySelector('#input-email');
+const selectHouse = document.querySelector('#house');
+
 buttonForm.addEventListener('click', () => {
   const loginValue = document.getElementById('form-login').value;
   const passwordValue = document.getElementById('form-password').value;
@@ -41,3 +47,21 @@ textAreaField.addEventListener('keyup', () => {
   const resultCounter = 500 - currentValue;
   counterField.innerHTML = resultCounter;
 });
+
+const createResultDiv = () => {
+  const newDiv = document.createElement('div');
+  newDiv.id = 'result-form-div';
+  newDiv.innerHTML += `Nome: ${firstNameInput.value} ${lastNameInput.value}`;
+  newDiv.innerHTML += `Email: ${emailInput.value}`;
+  newDiv.innerHTML += `Casa: ${selectHouse.value}`;
+  const inputFamily = document.querySelector('input[name="family"]:checked');
+  newDiv.innerHTML += `Família: ${inputFamily.value}`;
+  return newDiv;
+}
+
+submitButton.addEventListener('click', event => {
+  event.preventDefault();
+  const ResultDiv = createResultDiv();
+  formField.innerHTML = '';
+  formField.appendChild(ResultDiv);
+})
