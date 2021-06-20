5d06e6459053dd67561f1f0fae23c1059575f7ff
junglejf <jfcchagas@id.uff.br>
2021-06-19 23:52:14 -0300

    requisitos obrigatórios finalizados, falta melhorar o CSS e os 3 Bônus

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const loginUser = document.getElementById('login');
 const passwordUser = document.getElementById('senha');
 const buttonClick = document.getElementById('button1');
+const numRadioBtn = document.getElementById('avaliacao');
+const radioButtons = 10;
+const checkBtn = document.getElementById('agreement');
+const enviar = document.getElementById('submit-btn');
+
 
 function gerarAlerta() {
   if ((loginUser.value === 'tryber@teste.com') && (passwordUser.value === '123456')) {
@@ -10,4 +15,26 @@ function gerarAlerta() {
   }
 }
 
+const makeNumRadioBtn = (n) => {
+  for (let i = 0; i < n; i += 1) {
+    const radioBox = document.createElement('input');
+    radioBox.setAttribute('type', 'radio');
+    radioBox.setAttribute('name', 'rate');
+
+    const radioLabel = document.createElement('label');
+    const num = i + 1;
+    const radioName = num.toString();
+    radioBox.setAttribute('value', radioName);
+    radioLabel.setAttribute('for', radioName);
+    radioLabel.innerHTML = radioName;
+
+    numRadioBtn.appendChild(radioLabel);
+    numRadioBtn.appendChild(radioBox);
+
+  }
+}; makeNumRadioBtn(radioButtons);
+
+const submitBtnOn = () => { enviar.disabled = !(checkBtn.checked); };
+
 buttonClick.addEventListener('click', gerarAlerta);
+checkBtn.addEventListener('click', submitBtnOn);
