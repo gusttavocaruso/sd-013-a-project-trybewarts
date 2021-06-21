e5e2a1ffcc12095f98fcb0b71aefe54d0b666221
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-21 20:36:37 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inpuSenha = document.querySelector('#input-senha');
 const buttonLogin = document.querySelector('#button-enviar');
 const buttonEnviar = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
+const textArea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 function VerificaLogin() {
   const login = inpuLogin.value;
@@ -21,3 +23,11 @@ function activeButton() {
   }
 }
 checkbox.addEventListener('click', activeButton);
+
+function setCounterText() {
+  const textAreaText = textArea.value;
+  const counterText = 500 - textAreaText.length;
+  counter.innerText = counterText;
+}
+
+textArea.addEventListener('input', setCounterText);
