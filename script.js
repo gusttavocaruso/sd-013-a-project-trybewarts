b657f547b42e8d025f591aa1495cfbe902da7b32
Matheus <matheushtc25@gmail.com>
2021-06-21 19:48:20 -0300

    Finalizando todos requisitos e resolvendo todos erros do linter

--- script.js
+++ script.js
@@ -17,26 +17,59 @@ buttonHeader.addEventListener('click', verificarFormHeader);
 const submitBtn = document.getElementById('submit-btn');
 submitBtn.disabled = true;
 
+const inputAgreement = document.getElementById('agreement');
+
 function verificaCheck() {
   const conteudo = document.getElementById('agreement').value;
-  if (conteudo !== null && conteudo !== '') {
+  if (conteudo !== null || conteudo !== '') {
     submitBtn.disabled = false;
   } else {
     submitBtn.disabled = true;
   }
 }
 
-document.getElementById('agreement').addEventListener('input', verificaCheck);
+inputAgreement.addEventListener('input', verificaCheck);
 
-const textArea = document.getElementById('textarea');
-const cont = document.getElementById('counter');
+const textArea = document.querySelector('#textarea');
+const conter = document.querySelector('#counter');
 
 function contadorCaractere() {
-  const value = parseInt(textArea.value.length, 10);
-  let caracteresRestantes = 500;
-  const caracteresDigitados = value;
-  caracteresRestantes -= caracteresDigitados;
-  cont.innerText = caracteresRestantes;
+  const value = textArea.value.length;
+  const maxCaractere = 500;
+  conter.innerText = `${maxCaractere - value} caracteres restantes`;
 }
 
-textArea.addEventListener('keydown', contadorCaractere);
+textArea.addEventListener('input', contadorCaractere);
+
+function checkMaterias() {
+  const arrayMaterias = [];
+  const elemMaterias = document.querySelectorAll('.subject');
+  for (let index = 0; index < elemMaterias.length - 1; index += 1) {
+    if (elemMaterias[index].checked) {
+      arrayMaterias.push(` ${elemMaterias[index].value}`);
+    }
+  }
+  return arrayMaterias;
+}
+
+function newContent(event) {
+  event.preventDefault();
+  const form = document.querySelector('#evaluation-form');
+  const nome = document.querySelector('#input-name').value;
+  const sobrenome = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const casa = document.querySelector('#house').value;
+  const familia = document.querySelector('[name="family"]:checked').value;
+  const materias = checkMaterias();
+  const avaliacao = document.querySelector('[name="rate"]:checked').value;
+  const textAreavalue = document.querySelector('#textarea').value;
+  form.innerText = '';
+  const p = document.createElement('p');
+  form.appendChild(p);
+  p.classList.add('p-form');
+  p.innerHTML = `Nome: ${nome} ${sobrenome} </br> Email: ${email} </br> Casa: ${casa} </br> 
+  Família: ${familia} </br> Matérias: ${Object.values(materias)} </br> 
+  Avaliação: ${avaliacao} </br> Observações: ${textAreavalue}`;
+}
+
+submitBtn.addEventListener('click', newContent);
