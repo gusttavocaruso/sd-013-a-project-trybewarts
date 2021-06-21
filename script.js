a2301f6c80f390c39ecd6de66d2b3835f0e34644
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-21 13:34:21 -0300

    resolvendo requisito 21

--- script.js
+++ script.js
@@ -29,76 +29,53 @@ textArea.onkeyup = function Math() {
   counter.innerHTML = `${500 - this.value.length} restantes`;
 };
 
-const nome = document.getElementById("input-name");
-const sobrenome = document.getElementById("input-lastname");
-const email = document.getElementById("input-email");
-const casas = document.getElementById("house");
-const familias = document.querySelectorAll(".inputsFamilia");
-const conteudos = document.querySelectorAll(".subject");
-const nota = document.querySelectorAll(".inputsNota");
-
-function verificarNome () {
-  return nome.value;
-}
-
-function verificarSobrenome () {
-  return sobrenome.value;
-}
-
-function verificarEmail () {
-  return email.value;
-}
-
-function verificarFamilia () {
-  for (let i = 0 ; i < familias.length ; i += 1 ) {
-    if (familias[i].checked) {
-      return familias[i].value;
+function verificarFamilia() {
+  const family = document.getElementsByName('family');
+  for (let i = 0; i < family.length; i += 1) {
+    if (family[i].checked) {
+      return family[i].value;
     }
   }
 }
 
-function verificarCasa () {
-  for (let i = 0 ; i < casas.length ; i += 1 ) {
-    if (casas[i].checked) {
-      return casas[i].value;
+const house = document.querySelector('#house');
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+
+function verificarConteudos() {
+  const subjects = document.getElementsByClassName('subject');
+  let subjectsSelected = '';
+  for (let i = 0; i < subjects.length; i += 1) {
+    if (subjects[i].checked) {
+      subjectsSelected += `${subjects[i].value}, `;
+    }
+  }
+  return subjectsSelected;
+}
+
+function verificarAvaliacao() {
+  const evaluation = document.getElementsByName('rate');
+  for (let i = 0; i < evaluation.length; i += 1) {
+    if (evaluation[i].checked) {
+      return evaluation[i].value;
     }
   }
 }
 
-function verificarNota () {
-  for (let i = 0 ; i < nota.length ; i += 1 ) {
-    if (nota[i].checked) {
-      return nota[i].value;
-    }
-  }
-}
-
-function verificarConteudo () {
-  for (let i = 0 ; i < conteudos.length ; i += 1 ) {
-    if (conteudos[i].checked) {
-      return conteudos[i].value;
-    }
-  }
-}
-
-function trocarInputs () { 
-  const form = document.getElementsByTagName('form'); 
-  const familySelected = verificarFamilia(); 
-  const verificarConteudo = verificarConteudo(); 
-  const rateSelected = evaluationNps();
-  const verificarNome = verificarNome();
-  const verificarSobrenome = verificarSobrenome();
-  const verificarEmail = verificarEmail() 
-  const verificarCasa = verificarCasa();
-  const verificarNota = verificarNota();
-  
-  form.innerHTML = ''; 
-  form.innerHTML += `Nome: ${verificarNome} ${verificarSobrenome}`; 
-  form.innerHTML += `Email: ${verificarEmail}`; 
-  form.innerHTML += `Casa: ${verificarCasa}`; 
-  form.innerHTML += `Familia: ${familySelected}`; 
-  form.innerHTML += `Conteúdos: ${verificarConteudo}`; 
-  form.innerHTML += `NPS: ${verificarNota}`; 
+function trocarInputs() {
+  const familySelected = verificarFamilia();
+  const subjectsSelected = verificarConteudos();
+  const npsEvaluation = verificarAvaliacao();
+  const form = document.getElementsByTagName('form')[1];
+  form.innerHTML = '';
+  form.innerHTML += `Nome: ${nome.value} ${sobrenome.value} <br>`;
+  form.innerHTML += `Email: ${email.value} <br>`;
+  form.innerHTML += `Casa: ${house.value} <br>`;
+  form.innerHTML += `Família: ${familySelected} <br>`;
+  form.innerHTML += `Matérias: ${subjectsSelected} <br>`;
+  form.innerHTML += `Avaliação: ${npsEvaluation} <br>`;
+  form.innerHTML += `Observações: ${textArea.value}`;
 }
 
 sendbtn.addEventListener('click', trocarInputs);
