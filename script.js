da0e08fd82b1055b984a3b989a7457ee50892046
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-06-22 02:59:15 -0300

    JAVASCRIPT - Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const botao = document.querySelector('.botao');
 const senha = document.getElementById('senha');
 const login = document.getElementById('login');
+const submitBtn = document.getElementById('submit-btn');
 
 function loginOk() {
   if ((login.value === 'tryber@teste.com') && (senha.value === '123456')) {
@@ -10,3 +11,29 @@ function loginOk() {
   }
 }
 botao.addEventListener('click', loginOk);
+
+
+
+// function verifyAgreement() {
+//   const checkbox = document.querySelector('#agreement');
+//   const submitButton = document.querySelector('#submit-btn');
+
+//   checkbox.addEventListener('input', () => {
+//     const disabled = submitButton.hasAttribute('disabled');
+//     if (disabled) submitButton.removeAttribute('disabled');
+//   });
+// }
+
+// verifyAgreement();
+
+
+
+function test() {
+  if (agreement.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+agreement.addEventListener('click', test);
+console.log()
