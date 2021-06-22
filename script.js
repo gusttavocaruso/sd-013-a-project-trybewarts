0ce30ccf6a5a7109a82b853768809e623bcd8596
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-06-22 16:27:55 -0300

    requisitos do 5 ao 12

---
+++ script.js
@@ -0,0 +1,13 @@
+// // requisito 3
+// const login = document.querySelector('#login');
+// const senha = document.querySelector('#senha');
+// const enviar = document.querySelector('#enviar')
+
+// if (login === 'tryber@teste.com' && senha === '123456') {
+//   alert('Seja bem vindo')
+// }
+// else {
+//  alert('Login ou senha invalidos')
+// } 
+
+
