674a0329170b7997510a9f3dd04d688fcf83d87a
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-06-22 16:42:02 -0300

    ajeitando erros lint

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
-const buttonLogin = document.getElementById("entrar");
-const inputLogin = document.getElementById("login");
-const inputPassword = document.getElementById("senha");
-const buttonEnviar = document.getElementById("submit-btn");
-const checkboxAgreement = document.getElementById("agreement");
-const commentTextarea = document.getElementById("textarea");
+const buttonLogin = document.getElementById('entrar');
+const inputLogin = document.getElementById('login');
+const inputPassword = document.getElementById('senha');
+const buttonEnviar = document.getElementById('submit-btn');
+const checkboxAgreement = document.getElementById('agreement');
+const commentTextarea = document.getElementById('textarea');
 
-buttonLogin.addEventListener("click", () => {
+buttonLogin.addEventListener('click', () => {
   if (
-    inputLogin.value !== "tryber@teste.com" ||
-    inputPassword.value !== "123456"
+    inputLogin.value !== 'tryber@teste.com'
+    || inputPassword.value !== '123456'
   ) {
-    alert("Login ou senha inválidos.");
+    alert('Login ou senha inválidos.');
   } else {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   }
 });
 
-checkboxAgreement.addEventListener("click", () => {
+checkboxAgreement.addEventListener('click', () => {
   if (checkboxAgreement.checked === true) {
-    buttonEnviar.removeAttribute("disabled");
+    buttonEnviar.removeAttribute('disabled');
   } else {
-    buttonEnviar.setAttribute("disabled", "disabled");
+    buttonEnviar.setAttribute('disabled', 'disabled');
   }
 });
 
-commentTextarea.addEventListener("keyup", () => {
-  const spanCount = document.getElementById("counter");
-  let contador = commentTextarea.value.length;
+commentTextarea.addEventListener('keyup', () => {
+  const spanCount = document.getElementById('counter');
+  const contador = commentTextarea.value.length;
 
   if (commentTextarea.value === 0) {
-    spanCount.innerText = "500";
+    spanCount.innerText = '500';
   } else {
     spanCount.innerText = 500 - contador;
   }
 });
-
-// buttonEnviar.addEventListener("click", (e) => {
-//   // let inputName = document.getElementById("input-name");
-//   // let inputLastName = document.getElementById("input-lastname");
-//   // let inputEmail = document.getElementById("input-email");
-//   // let selectHouse = document.getElementById("house");
-//   // let radioFamily = document.querySelector("input[name=family]:checked");
-//   // let checkboxConteudo = document.querySelectorAll(".conteudo input:checked");
-//   // let radioNivelSatisfacao = document.querySelector("input[name=rate]:checked");
-//   // let textareaTextarea = document.getElementById("textarea");
-
-//   e.preventDefault();
-
-//   // inputName.value = inputName.value;
-//   // inputLastName.value = inputLastName.value;
-// });
