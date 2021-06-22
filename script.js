6b3cf496e66b4bb0a3e0c17b828cd68254cc2c58
Murilo Ribeiro <murilorsv14@gmail.com>
2021-06-21 21:37:55 -0300

    termino do dia 21

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const sendButton = () => {
       getEnviar.disabled = true;
     } else {
       getEnviar.disabled = false;
+      formsComplete
     }
   });
 };
@@ -35,37 +36,96 @@ const counterPad = () => {
 
 counterPad();
 
-function deleteMain() {
-  const form = document.querySelector('#evaluation-form');
-  form.parentNode.parentNode.removeChild(form.parentNode);
+function checkTrueCheckbox() {
+  const array = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
+  const arrayExit = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (document.getElementById(array[i]).checked === true) {
+      arrayExit.push(array[i]);
+    }
+  }
+  return arrayExit;
 }
 
-// const formsComplete = () => {
-//   const sendClick = document.getElementById('submit-btn');
-//   sendClick.addEventListener('click', () => {
+function checkTrueRadioButton() {
+  const familyRadio = document.getElementsByName('family');
+  let stringReturn = ''
+  for (let i = 0; i < familyRadio.length; i += 1) {
+    if (familyRadio[i].checked === true) {
+      stringReturn = familyRadio[i].value;
+    }
+  }
+  return stringReturn;
+}
 
-//     createNewTags();
-//   })
+function checkTrueRateButton() {
+  const familyRate = document.getElementsByName('rate');
+  let stringReturn = '';
+  for (let i = 0; i < familyRate.length; i += 1) {
+    if (familyRate[i].checked === true) {
+      stringReturn = familyRate[i].value;
+    }
+  }
+  return stringReturn;
+}
 
-// formsComplete();
+function getContent() {
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const casa = document.getElementById('house').value;
+  const familia = checkTrueRadioButton();
+  const materias = checkTrueCheckbox();
+  const avaliacao = checkTrueRateButton();
+  const observacoes = document.getElementById('textarea').value;
+  const array = [nome, sobrenome, email, casa, familia, materias, observacoes, avaliacao];
+  return array;
+}
 
-// const createNewTags = () => {
-//   const getMainContent = document.getElementsByClassName('mainClass');
-//   const getCheckboxesValue = document.querySelectorAll('input[name=mycheckboxes]:checked');
-//   const getFamilyValue = document.querySelectorAll('input[name=family]:checked');
-//   const getSiteRate = document.querySelector('input[name="rate"]:checked').value;
-//   const getFullName = document.querySelectorAll('input[name=name]').value;
-//   const getEmail = document.querySelector('input[name=email]').value;
-//   const getComents = document.querySelector('input[name=textbox]').value;
-//   const getHouse = document.querySelector('input[name=houses]').value;
+function deleteMainChilds() {
+  const main = document.querySelector('.mainClass');
+  const arrayOfNodes = main.childNodes;
+  main.removeChild(arrayOfNodes[1]);
+  main.removeChild(arrayOfNodes[2]);
+  main.removeChild(arrayOfNodes[4]);
+}
 
-//   const getMainContent = document.getElementsByClassName('mainClass');
-//   let newTag1 = document.createElement('div');
-//   let newTag2 = document.createElement('ul');
-//   for (let i = 0; i < 7; i += 1) {
-//     let newTag2 = document.createElement('li')[i];
-//   }
+function addDivOnMain() {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'resultado-forms');
+  document.querySelector('.mainClass').appendChild(div);
+}
 
-//   newTag1.innerHTML = 'Formulário'
-//   getMainContent.appendChild()
-// }
+function writeLines() {
+  const div = document.querySelector('#resultado-forms');
+  const newUl = document.createElement('ul');
+  div.appendChild(newUl);
+  for (let i = 0; i < 7; i += 1) {
+    const newLi = document.createElement('li');
+    newUl.appendChild(newLi);
+  }
+}
+
+function writeDivForms(array) {
+  const ul = document.getElementsByTagName('ul')[0];
+  ul[0].innerHTML = `Nome: ${array[0]} ${array[1]}`;
+  ul[1].innerHTML = `Email: ${array[2]}`;
+  ul[2].innerHTML = `Casa: ${array[3]}`;
+  ul[3].innerHTML = `Família: ${array[4]}`;
+  ul[4].innerHTML = `Matérias: ${array[5]}`;
+  ul[5].innerHTML = `Avaliação: ${array[7]}`;
+  ul[6].innerHTML = `Observações: ${array[6]}`;
+}
+
+// Fonte: Mateus Duarte
+
+const formsComplete = () => {
+  const sendClick = document.getElementById('submit-btn');
+  sendClick.addEventListener('click', () => {
+    const array = getContent();
+    deleteMainChilds();
+    addDivOnMain();
+    writeLines();
+    writeDivForms(array);
+  });
+};
