107a3b611485d4b159b500410ce2553cf30ca0d2
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-22 14:17:05 -0300

    tentando fazer a questao 21

--- script.js
+++ script.js
@@ -31,3 +31,37 @@ text.addEventListener('keyup', () => {
   const caracteres = text.value;
   counter.innerHTML = 500 - caracteres.length;
 });
+
+
+const nome1 = document.getElementById('input-name');
+const sobrenome1 = document.getElementById('input-lastname');
+const email1 = document.getElementById('input-email');
+const casa1 = document.getElementById('house');
+const familia1 = document.getElementById('familia');
+const materias1 = document.getElementById('materias');
+const avaliacao1 = document.getElementById('container-labelRate');
+const observacoes1 = document.getElementById('textarea');
+
+let nome2 = document.getElementById('nome2');
+let email2 = document.getElementById('email12');
+let casa2 = document.getElementById('casa2');
+let familia2 = document.getElementById('familia2');
+let materias2 = document.getElementById('materias2');
+let avaliacao2 = document.getElementById('avaliacao2');
+let observacoes2 = document.getElementById('observacoes2');
+
+const formulario1 = document.getElementById('evaluation-form');
+const formulario2 = document.getElementById('form-2');
+
+
+function resultForms() {
+
+  nome2.innerHTML = 'Nome:'+ nome1.value+' '+ nome2.value;
+  email2.innerHTML = 'Email: '+email1.value;
+  
+  formulario1.style.display = 'none';
+  formulario2.style.display = 'block';
+
+}
+
+buttonSubmit.addEventListener('click', resultForms);
