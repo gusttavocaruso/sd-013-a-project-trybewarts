43099a36b07a1dee65d94ca7a37aa5190a072e42
Cassyel <cassyel1967@gmail.com>
2021-06-21 23:32:13 -0300

    Realiza Requisitos 1-20, Formataçao ESlint CSS e JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-/* validação de form loguin  */
+/* validação de form login  */
 const email = document.getElementById('email-input');
 const senha = document.getElementById('senha-input');
-
-const botao = document.getElementById('btn');
+const button = document.getElementById('btn');
+const agreement = document.getElementById('agreement');
+const enviar = document.getElementById('submit-btn');
+const textArea = document.querySelector('#textarea');
+const counter = document.getElementById('counter');
 
 function validationLogin() {
   if ((email.value === 'tryber@teste.com') && (senha.value === '123456')) {
@@ -12,4 +15,20 @@ function validationLogin() {
   }
 }
 
-botao.addEventListener('click', validationLogin);
+function checaSubmit() {
+  if (agreement.checked === true) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+}
+
+function setCounter() {
+  const text = textArea.value;
+  const counterCharacter = '500' - text.length;
+  counter.innerText = counterCharacter;
+}
+
+textArea.addEventListener('input', setCounter);
+agreement.addEventListener('click', checaSubmit);
+button.addEventListener('click', validationLogin);
