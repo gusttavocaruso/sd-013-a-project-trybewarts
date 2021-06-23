59653ca33367aad95df3f88e13049167b96223b3
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-06-23 12:15:44 -0300

    conclusao do projeto

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function loginAuth() {
   const submitButton = document.querySelector('#login-button');
+  submitButton.addEventListener('click', (event) => {
+    const loginButton = document.querySelector('#login').value;
+    const passwordButton = document.querySelector('#password').value;
+    event.preventDefault();
 
-  submitButton.addEventListener('click', () => {
-    const loginButton = document.getElementById('login').value;
-    const passwordButton = document.getElementById('password').value;
-
-    if (loginButton.value == 'tryber@teste.com' && passwordButton.value == '123456') {
-      alert("Olá, Tryber!");
+    if (loginButton === 'tryber@teste.com' && passwordButton === '123456') {
+      alert('Olá, Tryber!');
     } else {
       alert('Login ou senha inválidos.');
     }
@@ -29,3 +29,16 @@ function enableButton() {
 }
 
 enableButton();
+
+function textareaCounter() {
+  const text = document.getElementById('textarea');
+  const counter = document.getElementById('counter');
+
+  text.addEventListener('keyup', () => {
+    const characterLength = text.value.length;
+    counter.setAttribute('value', (500 - characterLength));
+    counter.innerText = `${(counter.getAttribute('value'))}${'/500'}`;
+  });
+}
+
+textareaCounter();
